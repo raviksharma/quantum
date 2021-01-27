fcaedaa33736b7368fbc94fcdb587a1fab98f68d
Ravi Kant Sharma <rks1986@gmail.com>
2021-01-27 21:40:09 +0100

    using numpy kronecker product

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 :warning: This is not a tutorial. These notes are only for the author’s reference.
 
+* circuit model of computation
+* measurement in the computational basis
+
 ![Python application](https://github.com/raviksharma/quantum/workflows/Python%20application/badge.svg?branch=main)
 
 ## test
--- src/base.py
+++ src/base.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-#  computational basis state 0 - |0> (ket notation)
+# computational basis state 0 - |0> (ket notation)
 zero = np.array([[1],
                  [0]], dtype=np.complex)
 
-#  computational basis state 1 - |1>
+# computational basis state 1 - |1>
 one = np.array([[0],
                 [1]], dtype=np.complex)
 
@@ -13,106 +13,48 @@ one = np.array([[0],
 # where α, β ∈ Complex,
 # and |α|**2 + |β|**2 = 1, normalization constraint
 
-# |00>
+# |ab> = |a> kronecker_product |b>
+# a kronecker product is used to combine quantum states
+# https://en.wikipedia.org/wiki/Kronecker_product
+
+# |00> / vector representation
 zz = np.array([[1],
 	       [0],
 	       [0],
 	       [0]], dtype=np.complex)
 
+# or
+zz = np.kron(zero, zero)
+
 # |01>
-zo = np.array([[0],
-	       [1],
-	       [0],
-	       [0]], dtype=np.complex)
+zo = np.kron(zero, one)
 
 # |10>
-oz = np.array([[0],
-	       [0],
-	       [1],
-	       [0]], dtype=np.complex)
+oz = np.kron(one, zero)
 
 # |11>
-oo = np.array([[0],
-	       [0],
-	       [0],
-	       [1]], dtype=np.complex)
+oo = np.kron(one, one)
 
 # |000>
-zzz = np.array([[1],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [0]], dtype=np.complex)
+zzz = np.kron(zero, np.kron(zero, zero))
 
 # |001>
-zzo = np.array([[0],
-	        [1],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [0]], dtype=np.complex)
+zzo = np.kron(zero, np.kron(zero, one))
 
 # |010>
-zoz = np.array([[0],
-	        [0],
-	        [1],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [0]], dtype=np.complex)
+zoz = np.kron(zero, np.kron(one, zero))
 
 # |011>
-zoo = np.array([[0],
-	        [0],
-	        [0],
-	        [1],
-	        [0],
-	        [0],
-	        [0],
-	        [0]], dtype=np.complex)
+zoo = np.kron(zero, np.kron(one, one))
 
 # |100>
-ozz = np.array([[0],
-	        [0],
-	        [0],
-	        [0],
-	        [1],
-	        [0],
-	        [0],
-	        [0]], dtype=np.complex)
+ozz = np.kron(one, np.kron(zero, zero))
 
 # |101>
-ozo = np.array([[0],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [1],
-	        [0],
-	        [0]], dtype=np.complex)
+ozo = np.kron(one, np.kron(zero, one))
 
 # |110>
-ooz = np.array([[0],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [1],
-	        [0]], dtype=np.complex)
+ooz = np.kron(one, np.kron(one, zero))
 
 # |111>
-ooo = np.array([[0],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [0],
-	        [1]], dtype=np.complex)
+ooo = np.kron(one, np.kron(one, one))
